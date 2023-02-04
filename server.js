69d9ad7dc46655935bd366950b1b28498a316d33
rashy <yagoubi.r@gmail.com>
2023-02-04 09:56:18 +0100

    Add static public & simple routes

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 require('dotenv-flow').config(); // as soon as possible load env data
 const express = require("express");
 const app = express();
+const path= require("path");
 
 // Get env variable
 const { PORT, NODE_ENV } = process.env;
+
+//static ressources or public files middleware access > optionnal
+app.use('/',express.static(path.join(__dirname,'/public')))
+
+// Add Routing
+const router = require('./routes');
+app.use('/api', router);
 
 // Start Web API
 app.listen(PORT, (err) => {
