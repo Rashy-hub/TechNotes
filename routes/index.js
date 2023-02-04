9d1e43ae8d7cea85f1d6bdabae7c91cadaa0cc92
rashy <yagoubi.r@gmail.com>
2023-02-04 15:48:32 +0100

    Add: Custom middleware logger

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ const router = require('express').Router();
 const path = require('path');
 
 // add a default route to sendfile an html splah file, router can use regex to match url
-
 router.get('^/$|/index(.html)?',(req,res)=>{
-    
+
     res.sendFile(path.join(__dirname,'..','views','index.html'))
 }) 
 module.exports=router;
--- server.js
+++ server.js
@@ -2,20 +2,28 @@ require('dotenv-flow').config(); // as soon as possible load env data
 const express = require("express");
 const app = express();
 const path= require("path");
+const {eventLogger}=require('./middleware/logger')
 
 // Get env variable
 const { PORT, NODE_ENV } = process.env;
+//custom middleware event logger used here 
+app.use (eventLogger)
+//built in middleware that allows receiving/parsing JSON data
+app.use(express.json()); 
 
 //static ressources or public files middleware access > optionnal
 app.use('/',express.static(path.join(__dirname,'/public')))
 
 // Add Routing
 const router = require('./routes');
-app.use('/api', router); // adding api to the base url
+// adding api to the base url
+app.use('/api', router); 
+
 // At the end of the waterfall app.use , adding 404 not found redirection
 app.all('*',(req,res)=>{
-  res.status(404) // already set status to 404
-  //Then check if request accepts html or another type
+    // already set status to 404
+  res.status(404) 
+   //Then check if request accepts html or another type
   if(req.accepts('html')) {
     res.sendFile(path.join(__dirname,'views','404.html'))    
   } 
