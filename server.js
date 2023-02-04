e0203a1eb517fa0c936656d75031cc1561266992
rashy <yagoubi.r@gmail.com>
2023-02-04 10:21:50 +0100

    Add : 404 view and redirection

--- server.js
+++ server.js
@@ -12,6 +12,23 @@ app.use('/',express.static(path.join(__dirname,'/public')))
 // Add Routing
 const router = require('./routes');
 app.use('/api', router); // adding api to the base url
+// At the end of the waterfall app.use , adding 404 not found redirection
+app.all('*',(req,res)=>{
+  res.status(404) // already set status to 404
+  //Then check if request accepts html or another type
+  if(req.accepts('html')) {
+    res.sendFile(path.join(__dirname,'views','404.html'))    
+  } 
+  else if (req.accepts('json'))
+  {
+    res.json({message:'404 Not Found'})
+  }
+  else {
+    // if req accepts is not set , respond text type with 404 Not Found
+    res.type('txt').send('404 Not Found')
+  }
+
+})
 
 // Start Web API
 app.listen(PORT, (err) => {
